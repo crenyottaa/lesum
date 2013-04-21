040ce8cfc6825d616edf401bb448a76163d10620
Chuanlei Ren <cren@yottaa.com>
2013-04-21 18:45:27 +0800

    commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lesum::Application.config.secret_token = 'ccbec488e12b21e87fef8bc2317ed3af451a00ee29f6d1530fb163b4a08047b1c3967b874613ac7452c41f4da45fa684042d2629d75a23aa0b1c7c413af2fbd1'
